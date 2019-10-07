20cdc9f1bb7a1f2a4947387670f553833c4fe086
blockhead4 <blockhead4@naver.com>
2019-10-07 19:59:51 +0900

    add

---
+++ src/components/App.js
@@ -0,0 +1,23 @@
+import React from "react";
+
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      name: ""
+    };
+  }
+
+  render() {
+    return (
+      <div>
+        <button onClick={() => this.setState({ name: "jwp" })}>
+          Click Me!
+        </button>
+        <h1>Hello!@@!! {this.state.name}</h1>
+      </div>
+    );
+  }
+}
+
+export default App;
