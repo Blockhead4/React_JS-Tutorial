385d2c629a039355b10c5c09baf2628a46b2927d
blockhead4 <blockhead4@naver.com>
2019-10-07 22:51:15 +0900

    add

--- src/components/App.js
+++ src/components/App.js
@@ -1,22 +1,9 @@
 import React from "react";
+import Contact from "./Contact";
 
 class App extends React.Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      name: ""
-    };
-  }
-
   render() {
-    return (
-      <div>
-        <button onClick={() => this.setState({ name: "jwp" })}>
-          Click Me!
-        </button>
-        <h1>Hello!@@!! {this.state.name}</h1>
-      </div>
-    );
+    return <Contact />;
   }
 }
 
